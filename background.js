0638c06140622d861cfbe5299475c136b4a0d649
CW-B-W <cw.brad.wang@gmail.com>
2023-04-23 12:02:27 +0800

    remove unnecessary logging

--- background.js
+++ background.js
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.cookies.onChanged.addListener(function(info) {
-  console.log("onChanged" + JSON.stringify(info.cookie));
-});
+// chrome.cookies.onChanged.addListener(function(info) {
+//   console.log("onChanged" + JSON.stringify(info.cookie));
+// });
 
 /*function focusOrCreateTab(url) {
   chrome.windows.getAll({"populate":true}, function(windows) {
